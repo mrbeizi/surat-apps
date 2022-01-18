1bcf20283db778409c0a45a994e18c6810d89fcf
bei8 <bezi.zhong@gmail.com>
2022-01-18 16:24:57 +0700

    setting dan migrasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,74 +7,61 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Aplikasi Surat') }}</title>
+
+    <link href="https://getbootstrap.com/docs/4.6/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('template/plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('template/dist/css/adminlte.min.css')}}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style> .nama_pengguna { margin-bottom: 10px; } .kata_sandi { margin-bottom: 15px; } </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <div class="lockscreen-wrapper">
+        <div class="lockscreen-logo">
+          <a href=""><b>{{config('app.name')}}</b></a>
+        </div>
+        <div class="lockscreen-item">          
+          <div id="err" style="color: red">
+            @if(session()->has('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+          </div>
+      
+            <form class="sign-in-credentials" method="POST" action="{{route('login')}}">
+              {{csrf_field()}}
+              
+              <div class="login">
+                <input type="text" value="{{old('email')}}" name="email" class="nama_pengguna form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="Username" autofocus>
+                @if ($errors->has('email'))
+                  <div class="invalid-feedback">
+                    {{$errors->first('email')}}
+                  </div>          
+                @endif
+                <input type="password" name="password" class="kata_sandi form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" placeholder="Password">
+                @if ($errors->has('password'))
+                  <div class="invalid-feedback">
+                    {{$errors->first('password')}}
+                  </div>          
+                @endif
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+              </div>
+            </form>
+            <p class="mt-4 text-muted">&copy;{{date('Y')}} - {{config('app.name')}} Alright Reserved</p>
+          
+        </div>
+      </div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <script src="{{asset('template/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
 </body>
 </html>
