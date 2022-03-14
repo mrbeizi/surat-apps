573079f3d056f1a0139d4374ecf609032f977e3c
bei8 <bezi.zhong@gmail.com>
2022-03-14 20:04:13 +0700

    fix deleteAll features and methode

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -7,6 +7,9 @@ use App\Kategori;
 use Auth;
 use Session;
 use DataTables;
+use Response;
+use App\Exceptions\InvalidOrderException;
+use DB;
 
 class KategoriController extends Controller
 {
@@ -15,13 +18,15 @@ class KategoriController extends Controller
         $list_kategori = Kategori::all();        
         if($request->ajax()){
             return datatables()->of($list_kategori)
-            ->addColumn('action', function($data){
+                ->addColumn('action', function($data){
                     $button = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$data->id.'" data-original-title="Edit" class="edit btn btn-outline-success btn-sm edit-post"><i class="fa fa-pen"></i></a>';
                     $button .= '&nbsp;&nbsp;';
                     $button .= '<button type="button" name="delete" id="'.$data->id.'" class="delete btn btn-outline-danger btn-sm"><i class="fa fa-trash"></i></button>';
                     return $button;
+                })->addColumn('checkbox', function($data){
+                    return '<input type="checkbox" name="kategori_checkbox" data-id="'.$data->id.'"><label></label>';
                 })
-                ->rawColumns(['action'])
+                ->rawColumns(['action','checkbox'])
                 ->addIndexColumn(true)
                 ->make(true);
         }  
@@ -53,4 +58,62 @@ class KategoriController extends Controller
         $post = Kategori::where('id',$id)->delete();     
         return response()->json($post);
     }
+
+    public function deleteAll(Request $request)
+    {   
+
+        //return response()->json(count($request->id));
+        $g = 0;
+        for ($i=0; $i < count($request->id); $i++) { 
+            
+            $checkTot = DB::table('kategoris')
+            ->select('kategoris.nama_kategori')
+            ->join('inboxes','kategoris.id','=','inboxes.id_kategori')
+            ->where('kategoris.id','=',$request->input('id')[$g])
+            ->first();
+
+            $checkTot2 = DB::table('kategoris')
+            ->select('kategoris.nama_kategori')
+            ->join('inboxes','kategoris.id','=','inboxes.id_kategori')
+            ->where('kategoris.id','=',$request->input('id')[$g])
+            ->get();
+
+            if (count($checkTot2) == 0) {
+                $cek = DB::table('kategoris')->where('kategoris.id', '=', $request->input('id')[$g])->delete();
+            }else{
+                $cek = 0;
+                $tes[] = $checkTot->nama_kategori;
+            }
+
+        $g++;
+        }
+
+        if (!empty($tes)) {
+            if ($cek == 1) {
+                return Response::json(array('msg' => 'ketiga'), 200);
+            }elseif($cek == 0){
+                return Response::json(array('msg' => 'gagal'), 200);
+            }else{
+                return Response::json(array('msg' => 'gagal'), 200);
+            }
+        }else{
+            return Response::json(array('msg' => 'berhasil'), 200);
+        }
+    
+        return response()->json($tes);
+
+        /* Need Improvement */
+        
+        // $id   = $request->id;
+        // $post = Kategori::whereIn('id', $id)->delete();
+    
+        // if ($post) {
+        //     return Response::json(array('msg' => 'berhasil'), 200);
+        // }else{
+        //     //return response()->json('tes');
+        //     return Response::json(array('msg' => 'gagal'), 200);
+        // }
+               
+    }
+  
 }
